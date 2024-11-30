b10d4d5781b4a65ac22a836bf4928c233cf4c146
Jericho Ilanga <ilangajericho@gmail.com>
2024-12-01 03:30:10 +0800

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(25)->create();
+        DB::table('users')->insert([
+            'name' => 'acb',
+            'email' => 'acb@gmail.com',
+            'password' => Hash::make('asdfghjkl!'),
+            'role'=> 'system'
+        ]);
     }
 }
