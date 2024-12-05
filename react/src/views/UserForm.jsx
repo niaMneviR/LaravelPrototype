0fb43f1d9ce0c13777b5c2f0108d945bfbb5c8b3
Jericho Ilanga <ilangajericho@gmail.com>
2024-12-06 06:07:48 +0800

    Added function in front end to add user

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import axiosClient from "../axios-client"
+import Navigation from "../components/Navigation";
+import sysAdd from "../style/system-admin.module.css"
+import list from "../style/user-list.module.css";
 
 export default function UserForm(){
     const {id} = useParams()
@@ -67,29 +70,75 @@ export default function UserForm(){
 
     return(
         <>
-            {temp.id && <h1>Update User: {temp.name}</h1>}
-            {!temp.id && !loading && <h1>New User</h1>}
-            <div className="card animated fadeInDown">
-                {loading && (
-                    <div className="text-center">Loading . . .</div>
-                )}
-                {errors && <div className="alert">
-                        {Object.keys(errors).map(
-                            key=>(<p key={key}>{errors[key][0]}</p>)
-                        )}
+            <header id="Navigation_Container" className={sysAdd.header}>
+                <Navigation/>
+            </header>
+            <div className={sysAdd.Main}>
+                {temp.id && <h1 className={sysAdd.h1}>Update User: {temp.name}</h1>}
+                {!temp.id && !loading && <h1 className={sysAdd.h1}>New User</h1>}
+                <div>
+                    {!loading &&
+                    <form onSubmit={onSubmit}>
+                        <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th>Department</th>
+                                <th>Branch</th>
+                                <th>Status</th>
+                                <th>Password</th>
+                                <th>Confirm Password</th>
+                            </tr>
+                        </thead>
+                        {loading && 
+                        <tbody>
+                            <div className="text-center">Loading . . .</div>
+                        </tbody>
+                        }
+                        <tbody>
+                            <tr>
+                                <td className={list.input}>
+                                    <input type="text" value={user.name} onChange={ev => setUser({...user, name: ev.target.value})} placeholder="Name" />
+                                </td>
+                                <td>
+                                    <input type="email" value={user.email} onChange={ev => setUser({...user, email: ev.target.value})} placeholder="Email" />
+                                </td>
+                                <td>
+                                    <input type="text" value={user.role} onChange={ev => setUser({...user, role: ev.target.value})} placeholder="Role" />
+                                </td>
+                                <td>
+                                    <input type="text" value={user.department} onChange={ev => setUser({...user, department: ev.target.value})} placeholder="Department" />
+                                </td>
+                                <td>
+                                    <input type="text" value={user.branch} onChange={ev => setUser({...user, branch: ev.target.value})} placeholder="Branch" />
+                                </td>
+                                <td>
+                                    <input type="text" value={user.status} onChange={ev => setUser({...user, status: ev.target.value})} placeholder="Status" />
+                                </td>
+                                <td>
+                                    <input type="password" onChange={ev => setUser({...user, password: ev.target.value})} placeholder="Password" />
+                                </td>
+                                <td>
+                                    <input type="password" onChange={ev => setUser({...user, password_confirmation: ev.target.value})} placeholder="Confirm Password" />
+                                </td>
+                                <td><input type="submit" value="Save" /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </form>
+                    }
+                    {errors && <div className="alert">
+                    {Object.keys(errors).map(
+                        key=>(<p key={key}>{errors[key][0]}</p>)
+                    )}
                     </div>
                     }
-                    {!loading &&
-                        <form onSubmit={onSubmit}>
-                            <input type="text" value={user.name} onChange={ev => setUser({...user, name: ev.target.value})} placeholder="Name" />
-                            <input type="email" value={user.email} onChange={ev => setUser({...user, email: ev.target.value})} placeholder="Email" />
-                            <input type="password" onChange={ev => setUser({...user, password: ev.target.value})} placeholder="Password" />
-                            <input type="password" onChange={ev => setUser({...user, password_confirmation: ev.target.value})} placeholder="Confirm Password" />
-                        <button className="btn">Save</button>
-                        </form>
-                    
-                    }
+                </div>
             </div>
+
+           
         </>
     )
 }
