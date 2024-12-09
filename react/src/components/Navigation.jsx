79923800727bf6c4e822617e7d743c0f53556f58
RogelioGio <giotalingdan@gmail.com>
2024-12-09 11:50:35 +0800

    Refactor components and styles for improved readability and consistency

--- react/src/components/Navigation.jsx
+++ react/src/components/Navigation.jsx
@@ -17,7 +17,7 @@ export default function Navigation(){
         axiosClient.post('/logout').then(()=>{
             setUser({})
             setToken(null)
-            
+
         })
         navigate('/login');
     }
@@ -60,48 +60,48 @@ export default function Navigation(){
     return(
         <nav id={sysAdd.nav} className={sysAdd.nav}>
             <ul className={`${sysAdd.Navigations} ${sysAdd.ul}`} id="parent-nav">
-                <li id={sysAdd.logo}> <img src={smallLogo} alt="" className={`${sysAdd.logo} ${sysAdd.li}`}/></li>
-                <li id={sysAdd.home} className={sysAdd.li}> 
+                <li id={sysAdd.logo}> <img src={smallLogo} alt="" className={`${sysAdd.logo}`}/></li>
+                <li id={sysAdd.home} className={sysAdd.li}>
                     <Link to="/">
-                        <i className="fa-solid fa-house icon" id={sysAdd.icon} data-target="label1"></i> 
+                        <i className="fa-solid fa-house icon" id={sysAdd.icon} data-target="label1"></i>
                         <span className={sysAdd.label}  id="label1">Home</span>
                     </Link>
                 </li>
-                <li className={sysAdd.li}> 
+                <li className={sysAdd.li}>
                     <Link to="/courses">
-                        <i className="fa-solid fa-book icon"  id={sysAdd.icon} data-target="label2"></i> 
+                        <i className="fa-solid fa-book icon"  id={sysAdd.icon} data-target="label2"></i>
                         <span className={sysAdd.label}  id="label2">Course List Maintenance</span>
                     </Link>
                 </li>
-                <li className={sysAdd.li}> 
+                <li className={sysAdd.li}>
                     <Link to="/users">
-                        <i className="fa-solid fa-user-group icon" id={sysAdd.icon} data-target="label3"></i> 
+                        <i className="fa-solid fa-user-group icon" id={sysAdd.icon} data-target="label3"></i>
                         <span className={sysAdd.label}  id="label3">User Management</span>
                     </Link>
                 </li>
-                <li className={sysAdd.li}> 
-                    <i className="fa-solid fa-user-lock icon"  id={sysAdd.icon} data-target="label4"></i> 
+                <li className={sysAdd.li}>
+                    <i className="fa-solid fa-user-lock icon"  id={sysAdd.icon} data-target="label4"></i>
                     <span className={sysAdd.label}  id="label4">User Account Maintenance</span>
                 </li>
-                <li className={sysAdd.li}> 
-                    <i className="fa-solid fa-gears icon"  id={sysAdd.icon} data-target="label5"></i> 
+                <li className={sysAdd.li}>
+                    <i className="fa-solid fa-gears icon"  id={sysAdd.icon} data-target="label5"></i>
                     <span className={sysAdd.label}  id="label5">System Configuration Maintenance</span>
                 </li>
-                <li className={sysAdd.li}> 
-                    <i className="fa-solid fa-chart-pie icon"  id={sysAdd.icon} data-target="label6"></i> 
+                <li className={sysAdd.li}>
+                    <i className="fa-solid fa-chart-pie icon"  id={sysAdd.icon} data-target="label6"></i>
                     <span className={sysAdd.label}  id="label6">System Graph and Reports</span>
                 </li>
-                <li className={sysAdd.li}> 
-                    <i className="fa-solid fa-chart-gantt icon"  id={sysAdd.icon} data-target="label7"></i> 
-                    <span className={sysAdd.label}  id="label7">System Activity Logs</span>    
+                <li className={sysAdd.li}>
+                    <i className="fa-solid fa-chart-gantt icon"  id={sysAdd.icon} data-target="label7"></i>
+                    <span className={sysAdd.label}  id="label7">System Activity Logs</span>
                 </li>
             </ul>
             <ul className={`${sysAdd.users} ${sysAdd.yo} users`} id="ul-to-move">
                 <li className={sysAdd.li}> <i className="fa-solid fa-gear"></i></li>
                 <li className={sysAdd.li}> <i className="fa-solid fa-bell"></i> </li>
-                <li className={sysAdd.li}> 
+                <li className={sysAdd.li}>
                     <a href="#" onClick={onLogout} className="btn-logout">
-                        <i className="fa-solid fa-user"></i> 
+                        <i className="fa-solid fa-user"></i>
                     </a>
                 </li>
             </ul>
